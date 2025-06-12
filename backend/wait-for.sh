75b7db3680984152a14b516c9af467dc8a6df563
Leonardo-Cássio <leonardocassio110@gmail.com>
2025-06-11 21:59:03 -0300

    vapo

--- backend/wait-for.sh
+++ backend/wait-for.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Aguarda o MySQL estar disponível antes de iniciar o app
 
-echo "Aguardando MySQL em $DB_HOST:$DB_PORT..."
+echo "Aguardando MySQL em $1..."
 
-while ! nc -z $DB_HOST $DB_PORT; do
+while ! nc -z $1 $2; do
   sleep 1
 done
 
 echo "MySQL está pronto. Iniciando aplicação..."
-npm start
+exec "$@"
